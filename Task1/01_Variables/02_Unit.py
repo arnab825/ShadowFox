96b05329d19f3f87e522aebf370b9e9465108da1
Arnab Roy <rarnab225@gmail.com>
2024-05-04 14:15:27 +0530

    Removed Task directories

--- Task1/01_Variables/02_Unit.py
+++
@@ -1,4 +0,0 @@
-# 2. Create a variable called for and assign it a value 4.
-# See what happens and find out the reason behind the behavior that you see.
-for_value = 4
-print("Value of for value:", for_value)
